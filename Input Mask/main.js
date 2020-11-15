455618e89c2bbd5d88f33c325b1a9da255ceaebb
smeikx <smeikx@users.noreply.github.com>
2020-11-15 16:26:58 +0100

    implement reading of JSON files

--- Input Mask/main.js
+++ Input Mask/main.js
@@ -8,12 +8,20 @@ const contains_chinese = (string) => search(/[\u{4e00}-\u{9FFF}]/u) >= 0;
 
 
 // XXX: w → 21; y → 22
-const sound_indexes = {
-	// initial sounds
-	'b': 0, 'p': 1, 'm': 2, 'f': 3, 'd': 4, 't': 5, 'n': 6, 'l': 7, 'g': 8, 'k': 9, 'h': 10, 'z': 11, 'c': 12, 's': 13, 'zh': 14, 'ch': 15, 'sh': 16, 'r': 17, 'j': 18, 'q': 19, 'x': 20, 'w': 21, 'y': 22,
-	// final sounds
-	'a': 0, 'o': 1, 'e': 2, 'i': 3, 'er': 4, 'ai': 5, 'ei': 6, 'ao': 7, 'ou': 8, 'an': 9, 'en': 10, 'ang': 11, 'eng': 12, 'ong': 13, 'ia': 14, 'iao': 15, 'ie': 16, 'iu': 17, 'ian': 18, 'in': 19, 'iang': 20, 'ing': 21, 'iong': 22, 'u': 23, 'ua': 24, 'uo': 25, 'uai': 26, 'ui': 27, 'uan': 28, 'un': 29, 'uang': 30, 'ueng': 31, 'ü': 32, 'üe': 33, 'ue': 34, 'üan': 35, 'ün': 36 
-};
+const
+	initial_sounds = [
+		'b', 'p', 'm', 'f', 'd', 't', 'n', 'l', 'g', 'k', 'h', 'z', 'c', 's', 'zh', 'ch', 'sh', 'r', 'j', 'q', 'x', 'w', 'y'
+	],
+	final_sounds = [
+		'a', 'o', 'e', 'i', 'er', 'ai', 'ei', 'ao', 'ou', 'an', 'en', 'ang', 'eng', 'ong', 'ia', 'iao', 'ie', 'iu', 'ian', 'in', 'iang', 'ing', 'iong', 'u', 'ua', 'uo', 'uai', 'ui', 'uan', 'un', 'uang', 'ueng', 'ü', 'üe', 'ue', 'üan', 'ün'
+	],
+	sound_indexes = {
+		// initial sounds
+		'b': 0, 'p': 1, 'm': 2, 'f': 3, 'd': 4, 't': 5, 'n': 6, 'l': 7, 'g': 8, 'k': 9, 'h': 10, 'z': 11, 'c': 12, 's': 13, 'zh': 14, 'ch': 15, 'sh': 16, 'r': 17, 'j': 18, 'q': 19, 'x': 20, 'w': 21, 'y': 22,
+		// final sounds
+		'a': 0, 'o': 1, 'e': 2, 'i': 3, 'er': 4, 'ai': 5, 'ei': 6, 'ao': 7, 'ou': 8, 'an': 9, 'en': 10, 'ang': 11, 'eng': 12, 'ong': 13, 'ia': 14, 'iao': 15, 'ie': 16, 'iu': 17, 'ian': 18, 'in': 19, 'iang': 20, 'ing': 21, 'iong': 22, 'u': 23, 'ua': 24, 'uo': 25, 'uai': 26, 'ui': 27, 'uan': 28, 'un': 29, 'uang': 30, 'ueng': 31, 'ü': 32, 'üe': 33, 'ue': 34, 'üan': 35, 'ün': 36
+	};
+
 
 
 const assemble_pinyin = (() =>
@@ -301,6 +309,65 @@ document.getElementById('clear').addEventListener('click', () =>
 	create_entry();
 });
 
+
+{
+	const read_file = async (file) =>
+	{
+		return new Promise((resolve) =>
+		{
+			const reader = new FileReader();
+			reader.onloadend = () => resolve(reader.result);
+			reader.readAsText(file);
+		})
+	};
+
+	document.getElementById('upload-json').addEventListener('change', async (event) =>
+	{
+		const
+			files = event.target.files,
+			file_reader = new FileReader();
+
+		for (let i = files.length - 1; i >= 0; --i)
+		{
+			const
+				data = JSON.parse(await read_file(files[i])),
+				character_data = Object.entries(data.characters),
+				expression_data = data.expressions,
+				new_entries = [];
+
+			for (let i = character_data.length - 1; i >= 0; --i)
+			{
+				const
+					character = character_data[i][0],
+					values = character_data[i][1],
+					entry = new Entry('character'),
+					fields = entry.fields;
+				fields.character.value = character;
+				fields.pinyin.value = values[0] ?
+					initial_sounds[values[0]] + final_sounds[values[1]] :
+					final_sounds[values[1]];
+				fields.tone.value = values[2];
+				fields.translation.value = values[3];
+				fields.radicals.value = values[4];
+
+				new_entries.push(entry);
+			}
+			for (let i = expression_data.length - 1; i >= 0; --i)
+			{
+				const
+					values = expression_data[i],
+					entry = new Entry('expression'),
+					fields = entry.fields;
+				fields.expression.value = values[0];
+				fields.meaning.value = values[1];
+
+				new_entries.push(entry);
+			}
+			for (let i = new_entries.length - 1; i >= 0; --i)
+				ENTRIES_CONTAINER.appendChild(new_entries[i].element);
+		}
+	});
+}
 
 // restore cached values
 {
