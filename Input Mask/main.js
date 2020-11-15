f9c1a5f1cf386c872968a7276cb69ac9521cbbfb
smeikx <smeikx@users.noreply.github.com>
2020-11-15 01:08:00 +0100

    completle revamp data model
    Instead of fetching and processing DOM elements, a dedicated Entry class has been created.
    Entry instances contain the necessary information to keep track of and process form data more easily.

--- Input Mask/main.js
+++ Input Mask/main.js
@@ -1,18 +1,20 @@
 'use strict';
 
-// entry → a character or expression with all accompaning information
+// entry → a character, radical or expression with all accompaning information
 // field → one component of a field (e.g. ‘pinyin’ of a character)
 
 const remove_whitespace = (string) => string.replace(/\s/g, '');
 const contains_chinese = (string) => search(/[\u{4e00}-\u{9FFF}]/u) >= 0;
+
 
 // XXX: w → 21; y → 22
 const sound_indexes = {
 	// initial sounds
 	'b': 0, 'p': 1, 'm': 2, 'f': 3, 'd': 4, 't': 5, 'n': 6, 'l': 7, 'g': 8, 'k': 9, 'h': 10, 'z': 11, 'c': 12, 's': 13, 'zh': 14, 'ch': 15, 'sh': 16, 'r': 17, 'j': 18, 'q': 19, 'x': 20, 'w': 21, 'y': 22,
 	// final sounds
-	'a': 0, 'o': 1, 'e': 2, 'i': 3, 'er': 4, 'ai': 5, 'ei': 6, 'ao': 7, 'ou': 8, 'an': 9, 'en': 10, 'ang': 11, 'eng': 12, 'ong': 13, 'ia': 14, 'iao': 15, 'ie': 16, 'iu': 17, 'ian': 18, 'in': 19, 'iang': 20, 'ing': 21, 'iong': 22, 'u': 23, 'ua': 24, 'uo': 25, 'uai': 26, 'ui': 27, 'uan': 28, 'un': 29, 'uang': 30, 'ueng': 31, 'ü': 32, 'üe': 33, 'üan': 34, 'ün': 35 
+	'a': 0, 'o': 1, 'e': 2, 'i': 3, 'er': 4, 'ai': 5, 'ei': 6, 'ao': 7, 'ou': 8, 'an': 9, 'en': 10, 'ang': 11, 'eng': 12, 'ong': 13, 'ia': 14, 'iao': 15, 'ie': 16, 'iu': 17, 'ian': 18, 'in': 19, 'iang': 20, 'ing': 21, 'iong': 22, 'u': 23, 'ua': 24, 'uo': 25, 'uai': 26, 'ui': 27, 'uan': 28, 'un': 29, 'uang': 30, 'ueng': 31, 'ü': 32, 'üe': 33, 'ue': 34, 'üan': 35, 'ün': 36 
 };
+
 
 const assemble_pinyin = (() =>
 {
@@ -47,207 +49,242 @@ const split_pinyin = (pinyin) =>
 };
 
 
-const templates = Object.freeze(
+let ENTRY_COUNT = 0; // also serves as ID for every new entry
+const ENTRIES = []; // collects all Entry() objects
+const ENTRIES_CONTAINER = document.getElementById('entries');
+
+
+/*
 {
-	entry: (() =>
-	{
-		const entry = document.createElement('li');
-		entry.className = 'entry';
-		entry.innerHTML =
-		`<label for='characters'>Characters</label>
-		<input class='character' name='characters'>`;
-		return entry;
-	})(),
+	element: , // <li class='entry'>
+	fields: , // generated list of all input fields
+	type: , // character || expression
+	id: , //	for retrieving from ENTRIES and backup
+	primary_field: , // the first field
+	contextual_fields: , // the fields that change depending on type
+	switch_type: function (fields) {}, // function to switch type
+};
+*/
 
-	character: (() =>
-	{
-		const container = document.createElement('div');
-		container.innerHTML =
-		`<label for='pinyin'>Pinyin</label>
-		<input class='pinyin' name='pinyin' minlength='1' size='6' maxlength='6'>
-		<label for='tone'>Tone</label>
-		<input class='tone' type='number' name='tone' min='0' max='4' size='1'>
-		<label for='translation'>Translation</label>
-		<input class='translation' name='translation'>
-		<label for='radicals'>Radicals</label>
-		<input class='radicals' name='radicals' size='5' maxlength='5'>`;
-		return container;
-	})(),
-
-	expression: (() =>
-	{
-		const container = document.createElement('div');
-		container.innerHTML =
-		`<label for='meaning'>Meaning</label>
-		<input class='meaning' name='meaning'>`;
-		return container;
-	})(),
-
-	get_copy_of: function (type)
-	{
-		return this[type].cloneNode(true);
-	}
-});
-
-
-let entry_count = 0;
-
-const create_entry = (type = 'character') =>
+const Entry = (() =>
 {
-	const
-		entry = templates.get_copy_of('entry'),
-		character_inputs = templates.get_copy_of('character'),
-		expression_inputs = templates.get_copy_of('expression'), // TODO: maybe generate on demand
-		first_field = entry.getElementsByTagName('input')[0],
-		backup_type_key =
-			(sessionStorage.entry_count =
-			entry.dataset.index =
-			character_inputs.dataset.index =
-			expression_inputs.dataset.index = entry_count++) + 'type';
-
-	let previous_length = 0;
-	first_field.addEventListener('input', (event) =>
+	const pool = Object.seal(
 	{
-		const current_length = event.target.value.length;
-		if (previous_length <= 1 && current_length > 1)
+		character: [],
+		expression: [],
+		take: function (type)
 		{
-			// entry describes an expression
-			entry.replaceChild(expression_inputs, character_inputs);
-			first_field.classList.replace('character', 'expression');
-			sessionStorage[backup_type_key] = 'expression';
-		}
-		else if (current_length <= 1 && previous_length > 1)
+			return this[type].pop();
+		},
+		give: function (type, element)
 		{
-			// entry describes a single character
-			entry.replaceChild(character_inputs, expression_inputs);
-			first_field.classList.replace('expression', 'character');
-			sessionStorage[backup_type_key] = 'character';
+			this[type].push(element);
+			return element;
 		}
-		previous_length = current_length;
 	});
 
-	// per default, the entry describes a single character
-	entry.appendChild(type == 'character' ? character_inputs : expression_inputs);
-	sessionStorage[backup_type_key] = type;
-	return entry;
-};
-
-
-const entries = document.getElementById('entries');
-
-// restore entries from this session
-{
-	const cached_entry_count = sessionStorage.entry_count;
-	if (cached_entry_count)
+	// Create an object containing all the fields necessary for the specified type.
+	const Contextual_Fields = (() =>
 	{
-
-		const create_character_entry = (id) =>
+		return function (type)
 		{
-			const
-				entry = create_entry(),
-				inputs = entry.getElementsByTagName('input');
+			const container = this.element = document.createElement('div');
+			this.fields = {};
+			this.type = type;
+			let label;
 
-			inputs[0].value = sessionStorage[id + 'character'];
-			inputs[1].value = sessionStorage[id + 'pinyin'];
-			inputs[2].value = sessionStorage[id + 'tone'];
-			inputs[3].value = sessionStorage[id + 'meaning'];
-			inputs[4].value = sessionStorage[id + 'radicals'];
+			if (type == 'character')
+			{
+				label = container.appendChild(document.createElement('label'));
+				label.for = 'pinyin';
+				label.innerText = 'Pinyin';
 
-			return entry;
-		};
+				const pinyin = this.fields.pinyin = container.appendChild(document.createElement('input'));
+				pinyin.className = pinyin.name = 'pinyin';
+				pinyin.size = pinyin.minlength = pinyin.maxlength = 1;
 
-		const create_expression_entry = (id) =>
-		{
-			const
-				entry = create_entry('expression'),
-				inputs = entry.getElementsByTagName('input');
 
-			inputs[0].value = sessionStorage[id + 'expression'];
-			inputs[1].value = sessionStorage[id + 'translation'];
+				label = container.appendChild(document.createElement('label'));
+				label.for = 'tone';
+				label.innerText = 'Tone';
 
-			return entry;
-		};
+				const tone = this.fields.tone = container.appendChild(document.createElement('input'));
+				tone.className = tone.name = 'tone';
+				tone.type = 'number';
+				tone.min = 0;
+				tone.max = 4;
+				tone.size = 1;
 
-		for (let entry, i = 0; i <= cached_entry_count; ++i)
-		{
-			if (sessionStorage[i + 'type'] == 'character')
-				entry = create_character_entry(i);
-			else
-				entry = create_expression_entry(i);
-			console.log('sad', entry);
-			//entries.appendChild(entry);
+
+				label = container.appendChild(document.createElement('label'));
+				label.for = 'translation';
+				label.innerText = 'Translation';
+
+				const translation = this.fields.translation = container.appendChild(document.createElement('input'));
+				translation.className = translation.name = 'translation';
+
+
+				label = container.appendChild(document.createElement('label'));
+				label.for = 'radicals';
+				label.innerText = 'Radicals';
+
+				const radicals = this.fields.radicals = container.appendChild(document.createElement('input'));
+				radicals.className = radicals.name = 'radicals';
+				radicals.size = radicals.maxlength = 5;
+			}
+			else // type == 'expression'
+			{
+				label = container.appendChild(document.createElement('label'));
+				label.for = 'meaning';
+				label.innerText = 'Meaning';
+
+				const meaning = this.fields.meaning = container.appendChild(document.createElement('input'));
+				meaning.className = meaning.name = 'meaning';
+			}
 		}
-	}
-}
+	})();
 
-
-/* assign function to add new input elements */
-{
-	const record_backup = (event) =>
+	// sets dataset.id of all fields
+	const update_ids = function (entry)
 	{
-		const
-			target =	event.target,
-			entry = target.parentElement;
-		sessionStorage[entry.dataset.index+target.className] = target.value;
+		const fields = Object.values(entry.fields);
+		for (let i = fields.length; i > 0; fields[--i].dataset.id = entry.id);
 	};
 
-	document.getElementById('create-new-entry').addEventListener('click', () =>
+	// either takes an object from the pool or creates a new one
+	const get_contextual_fields = (type) => pool.take(type) || new Contextual_Fields(type);
+	
+	// set this.fields to comprise this.primary_field and this.contextual_fields.fields
+	const update_fields = (entry) =>
+		entry.fields = Object.assign({[entry.type]: entry.primary_field}, entry.contextual_fields.fields);
+
+	const shall_switch = (current_char_count, previous_char_count) =>
+		(previous_char_count > 1 && current_char_count <= 1) ||
+		(previous_char_count <= 1 && current_char_count > 1);
+
+	// changes the type and replaces all accompanying fields
+	const switch_type = function ()
 	{
-		const new_entry = entries.appendChild(create_entry());
-		new_entry.getElementsByTagName('input')[0].focus();
-		new_entry.addEventListener('change', record_backup);
-	});
+		const
+			old_type = this.type,
+			old_contextual_fields = pool.give(old_type, this.contextual_fields);
+
+		// clear values
+		const fields = Object.values(old_contextual_fields.fields);
+		for (let i = fields.length; i > 0; fields[--i].value = '');
+
+		this.type =
+			this.primary_field.name =
+				sessionStorage[this.id + 'type'] =
+					old_type == 'character' ? 'expression' : 'character';
+		this.primary_field.classList.replace(old_type, this.type);
+
+		this.contextual_fields = get_contextual_fields(this.type);
+
+		update_fields(this);
+		update_ids(this);
+
+		this.element.replaceChild(this.contextual_fields.element, old_contextual_fields.element);
+	};
+
+	return function ()
+	{
+		const
+			container = this.element = document.createElement('li'),
+			primary_field = this.primary_field = container.appendChild(document.createElement('input')),
+			contextual_fields = this.contextual_fields = get_contextual_fields('character');
+
+		primary_field.className = primary_field.name = 'character';
+
+		let current_char_count, previous_char_count = 0;
+		primary_field.addEventListener('input', (event) =>
+		{
+			current_char_count = event.target.value.length;
+			if (shall_switch(current_char_count, previous_char_count))
+				this.switch_type();
+			previous_char_count = current_char_count;
+		});
+
+		this.switch_type = switch_type;
+
+		this.type = 'character';
+		update_fields(this);
+
+		this.id = ENTRY_COUNT++;
+		update_ids(this);
+
+		sessionStorage[this.id + 'type'] = 'character';
+		sessionStorage.ENTRY_COUNT = this.id + 1;
+
+		this.element.appendChild(contextual_fields.element);
+	}
+})();
+
+
+// assign function to add new input elements
+{
+	const save_field = (event) =>
+	{
+		const
+			target = event.target,
+			id = target.dataset.id;
+		sessionStorage[id + target.name] = target.value;
+	};
+
+	ENTRIES_CONTAINER.addEventListener('change', save_field);
 }
+
+
+const create_entry = () =>
+{
+	const new_entry = new Entry();
+	ENTRIES.push(new_entry);
+	ENTRIES_CONTAINER.appendChild(new_entry.element);
+	new_entry.primary_field.focus();
+};
+
+document.getElementById('create-new-entry').addEventListener('click', create_entry);
+
 
 // assign function to download a JSON file
 document.getElementById('create-json').addEventListener('click', () =>
 {
-	const result = {};
-	const expressions = entries.getElementsByClassName('expression');
+	const result =
+	{
+		characters: {},
+		expressions: []
+	};
 
-	/* process character entries */ {
+	for (let i = ENTRIES.length - 1; i >= 0; --i)
+	{
 		const
-			characters = entries.getElementsByClassName('character'),
-			pinyin = entries.getElementsByClassName('pinyin'),
-			tones = entries.getElementsByClassName('tone'),
-			translations = entries.getElementsByClassName('translation'),
-			radicals = entries.getElementsByClassName('radicals'),
-			processed_entries = {};
+			entry = ENTRIES[i],
+			fields = entry.fields;
 
-		for (let i = characters.length - 1; i >= 0; --i)
+		if (entry.type == 'character')
 		{
-			const {initial, final} = split_pinyin(pinyin[i].value);
-
-			processed_entries[characters[i].value] = [
+			const {initial, final} = split_pinyin(fields.pinyin.value);
+			result.characters[fields.character.value] =
+			[
 				sound_indexes[initial],
 				sound_indexes[final],
-				tones[i].valueAsNumber,
-				translations[i].value,
-				radicals[i].value
+				fields.tone.valueAsNumber,
+				fields.translation.value,
+				fields.radicals.value
 			];
 		}
-
-		result.characters = processed_entries;
-	}
-	/* process expression entries */ {
-		const
-			expressions = entries.getElementsByClassName('expression'),
-			meanings = entries.getElementsByClassName('meaning'),
-			count = expressions.length,
-			processed_entries = new Array(count);
-
-		for (let i = 0; i < count; ++i)
-			processed_entries[i] = [ expressions[i].value, meanings[i].value ];
-
-		result.expressions = processed_entries;
+		else // entry.type == 'expression'
+			result.expressions.push(
+				[entry.fields.expression.value, entry.fields.meaning.value]);
 	}
 
-	const data = new Blob([JSON.stringify(result)], {type: 'text/plain;charset=utf-8'}),
+	const
+		data = new Blob([JSON.stringify(result)], {type: 'text/plain;charset=utf-8'}),
 		url = window.URL.createObjectURL(data),
 		anchor = document.createElement('a');
 
 	anchor.href = url;
-	anchor.download = 'static.txt';
+	anchor.download = 'Vocabulary.json';
 	anchor.click();
 
 	window.URL.revokeObjectURL(url);
@@ -257,7 +294,38 @@ document.getElementById('create-json').addEventListener('click', () =>
 document.getElementById('clear').addEventListener('click', () =>
 {
 	sessionStorage.clear();
-	entry_count = 0;
-	entries.innerHTML = '';
-	entries.appendChild(create_entry());
+	ENTRY_COUNT = ENTRIES.length = 0;
+	// TODO: Return Elements to Pool, maybe extend Pool for primary_fields
+	ENTRIES_CONTAINER.innerHTML = '';
+	create_entry();
 });
+
+
+// restore cached values
+{
+	const cached_entry_count = sessionStorage.ENTRY_COUNT;
+	if (cached_entry_count)
+	{
+		for (let id = 0; id < cached_entry_count; ++id)
+		{
+			const
+				type = sessionStorage[id + 'type'],
+				new_entry = new Entry();
+			// TODO: modify Entry() to take type as argument
+
+			ENTRIES.push(new_entry);
+
+			if (type == 'expression')
+				new_entry.switch_type();
+
+			const fields = Object.entries(new_entry.fields);
+			for (let i = fields.length - 1; i >= 0; --i)
+			{
+				const [type, field] = fields[i];
+				field.value = sessionStorage[id + type] || '';
+			}
+			ENTRIES_CONTAINER.appendChild(new_entry.element);
+		}
+	}
+	else create_entry();
+}
